c09270fea03b077f951cc4deb2e1159eaab5ce31
mraspovic <mate.raspovic@googlemail.com>
2025-01-01 14:17:32 +0100

    Quartz sync: Jan 1, 2025, 2:17 PM

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -208,16 +208,15 @@ export function renderPage(
       ))}
     </div>
   )
-
+/*
   const RightComponent = (
-    /*
     <div class="right sidebar">
       {right.map((BodyComponent) => (
         <BodyComponent {...componentData} />
       ))}
     </div>
-    */
   )
+*/
 
   const lang = componentData.fileData.frontmatter?.lang ?? cfg.locale?.split("-")[0] ?? "en"
   const doc = (
@@ -248,7 +247,7 @@ export function renderPage(
                 ))}
               </div>
             </div>
-            {RightComponent}
+            {/* {RightComponent} */}
             <Footer {...componentData} />
           </Body>
         </div>
