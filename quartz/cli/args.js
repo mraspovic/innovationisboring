7d6ade891a3d695d73d1309ed093186225c58c85
mraspovic <mate.raspovic@googlemail.com>
2025-01-01 17:02:33 +0100

    Quartz sync: Jan 1, 2025, 5:02 PM

--- quartz/cli/args.js
+++ quartz/cli/args.js
@@ -2,7 +2,7 @@ export const CommonArgv = {
   directory: {
     string: true,
     alias: ["d"],
-    default: "content",
+    default: "Innovation is Boring",
     describe: "directory to look for content files",
   },
   verbose: {
