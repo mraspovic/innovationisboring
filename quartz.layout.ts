bf95211fd8a978c77c9d875f1226abbda431ab91
mraspovic <mate.raspovic@googlemail.com>
2025-01-01 12:47:09 +0100

    Quartz sync: Jan 1, 2025, 12:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,11 +37,13 @@ export const defaultContentPageLayout: PageLayout = {
     */
     Component.DesktopOnly(Component.Explorer()),
   ],
+  /*
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
+  */
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
